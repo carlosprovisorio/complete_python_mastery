b070ca490d7c5da461d2f9955f140b07eaa961ef
Carlos <carlosprovisorio@icloud.com>
2024-07-19 13:29:18 -0400

    Ternary Operator

--- control_flow.py
+++ control_flow.py
@@ -1,3 +1,5 @@
+# Comparison Operators
+
 # print(10 > 3)
 # print(10 >= 3)
 # print(10 < 20)
@@ -6,13 +8,33 @@
 # print(10 == "10")
 # print(10 != 10)
 
-temperature = 15
 
-if temperature > 30:
-    print("It's warm")
-    print("Drink water")
-elif temperature > 20:
-    print("It's nice")
-else:
-    print("It's cold")
-print("Done")
+# Conditional Statement
+# temperature = 15
+
+# if temperature > 30:
+#     print("It's warm")
+#     print("Drink water")
+# elif temperature > 20:
+#     print("It's nice")
+# else:
+#     print("It's cold")
+# print("Done")
+
+# age = 22
+# if age >= 18:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+# age = 40
+# if age >= 30:
+#     message = "Eligible"
+# else:
+#     message = "Not Eligible"
+# print(message)
+
+# Ternary Operator
+age = 15
+message = "Eligible" if age >= 18 else "Not Eligible"
+print(message)
