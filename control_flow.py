91deb933368785fd9c88b8fdf2f36582bf743f64
Carlos <carlosprovisorio@icloud.com>
2024-07-19 13:39:28 -0400

    Logical Operators

--- control_flow.py
+++ control_flow.py
@@ -34,7 +34,43 @@
 #     message = "Not Eligible"
 # print(message)
 
-# Ternary Operator
-age = 15
-message = "Eligible" if age >= 18 else "Not Eligible"
-print(message)
+# # Ternary Operator
+# age = 15
+# message = "Eligible" if age >= 18 else "Not Eligible"
+# print(message)
+
+# Logical Operators
+
+# high_income = True
+# good_credit = True
+
+# if high_income and good_credit:
+#     print("Eligible")
+
+# high_income = False
+# good_credit = True
+
+# if high_income or good_credit:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+
+# high_income = False
+# good_credit = True
+# student = True
+
+# if not student:
+#     print("Eligible")
+# else:
+#     print("Not Eligible")
+
+
+high_income = False
+good_credit = True
+student = False
+
+if (high_income or good_credit) and not student:
+    print("Eligible")
+else:
+    print("Not Eligible")
