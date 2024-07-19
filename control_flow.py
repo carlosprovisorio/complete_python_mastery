3aac17eb5f7b33cea900d28ae4ff61b7965a26b7
Carlos <carlosprovisorio@icloud.com>
2024-07-19 13:20:28 -0400

    Conditional Statement

--- control_flow.py
+++ control_flow.py
@@ -1,7 +1,18 @@
-print(10 > 3)
-print(10 >= 3)
-print(10 < 20)
-print(10 <= 20)
-print(10 == 10)
-print(10 == "10")
-print(10 != 10)
+# print(10 > 3)
+# print(10 >= 3)
+# print(10 < 20)
+# print(10 <= 20)
+# print(10 == 10)
+# print(10 == "10")
+# print(10 != 10)
+
+temperature = 15
+
+if temperature > 30:
+    print("It's warm")
+    print("Drink water")
+elif temperature > 20:
+    print("It's nice")
+else:
+    print("It's cold")
+print("Done")
