17c330e3b2f5c16838a5e3217378f76467c86e98
Carlos <carlosprovisorio@icloud.com>
2024-07-23 07:50:37 -0400

    xargs

--- functions.py
+++ functions.py
@@ -1,7 +1,47 @@
 # Defining functions
-def greet(first_name, last_name):
-    print(f"Hi {first_name} {last_name}")
-    print("Welcome aboard")
+# Arguments
+# def greet(first_name, last_name):
+#     print(f"Hi {first_name} {last_name}")
+#     print("Welcome aboard")
 
 
-greet("Carlos", "Ferreira")
+# greet("Carlos", "Ferreira")
+
+
+# Type of functions
+# 1 - Perform a task
+# 2 - Return a value
+
+
+# def get_greeting(name):
+#     return f"Hi {name}"
+
+
+# message = get_greeting("Carlos")
+# print(message)
+
+
+# def increment(number, by):
+#     return number + by
+
+
+# print(increment(2, by=1))
+
+
+# Default Arguments
+# def increment(number, by=1):
+#     return number + by
+
+
+# print(increment(2, 5))
+
+
+# Xargs
+# def multiply(*numbers):
+#     total = 1
+#     for number in numbers:
+#         total *= number
+#     return total
+
+
+# print(multiply(2, 3, 4, 5))
