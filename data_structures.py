899d4c605d846b655f8234dec193771004c4e38f
Carlos <carlosprovisorio@icloud.com>
2024-07-24 13:35:45 -0400

    lists

---
+++ data_structures.py
@@ -0,0 +1,9 @@
+# lists
+letters = ["a", "b", "c"]  # list
+matrix = [[0, 1], [2, 3]]  # two dimensional list
+zeros = [0] * 5
+combined = zeros + letters
+numbers = list(range(20))
+chars = list("Hello World")
+print(chars)
+print(len(chars))
