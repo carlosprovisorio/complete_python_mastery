bed50c1f911c30a7b810c92a0a446b50bdff91d3
Carlos <carlosprovisorio@icloud.com>
2024-07-31 21:03:10 -0400

    The with Statement

--- exceptions.py
+++ exceptions.py
@@ -10,6 +10,7 @@
 #     print("No exceptions were thrown.")
 # print("Execution continues.")
 
+# Handling Exceptions
 
 # try:
 #     age = int(input("Age: "))
@@ -19,14 +20,28 @@
 # else:
 #     print("No exceptions were thrown.")
 
+# cleaning Up
 
+# try:
+#     file = open("exceptions.py")
+#     age = int(input("Age: "))
+#     xfactor = 10 / age
+# except (ValueError, ZeroDivisionError):
+#     print("You didn't enter a valid age.")
+# else:
+#     print("No exceptions were thrown.")
+# finally:
+#     file.close()
+
+
+# The with Statement
 try:
-    file = open("exceptions.py")
+    with open("exceptions.py") as file:
+        print("File opened.")
+
     age = int(input("Age: "))
     xfactor = 10 / age
 except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age.")
 else:
     print("No exceptions were thrown.")
-finally:
-    file.close()
