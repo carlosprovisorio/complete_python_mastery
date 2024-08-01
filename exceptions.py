c854c3991586a0fabec86d58c58faa3c0a98845c
Carlos <carlosprovisorio@icloud.com>
2024-07-31 21:09:19 -0400

    Raising Exceptions

--- exceptions.py
+++ exceptions.py
@@ -35,13 +35,25 @@
 
 
 # The with Statement
-try:
-    with open("exceptions.py") as file:
-        print("File opened.")
+# try:
+#     with open("exceptions.py") as file:
+#         print("File opened.")
 
-    age = int(input("Age: "))
-    xfactor = 10 / age
-except (ValueError, ZeroDivisionError):
-    print("You didn't enter a valid age.")
-else:
-    print("No exceptions were thrown.")
+#     age = int(input("Age: "))
+#     xfactor = 10 / age
+# except (ValueError, ZeroDivisionError):
+#     print("You didn't enter a valid age.")
+# else:
+#     print("No exceptions were thrown.")
+
+
+def calculate_xfactor(age):
+    if age <= 0:
+        raise ValueError("Age cannot be 0 or less.")
+    return 10 / age
+
+
+try:
+    calculate_xfactor(-1)
+except ValueError as error:
+    print(error)
