b71354a7d016b067b337922e9196b145eac24e19
Carlos <carlosprovisorio@icloud.com>
2024-07-31 20:49:56 -0400

    Exceptions

--- exceptions.py
+++ exceptions.py
@@ -2,10 +2,19 @@
 # numbers = [1, 2]
 # print(numbers[3])  # it generates an error by the developer
 
+# try:
+#     age = int(input("Age: "))
+# except ValueError:
+#     print("You didn't enter a valid age.")
+# else:
+#     print("No exceptions were thrown.")
+# print("Execution continues.")
+
+
 try:
     age = int(input("Age: "))
-except ValueError:
+    xfactor = 10 / age
+except (ValueError, ZeroDivisionError):
     print("You didn't enter a valid age.")
 else:
     print("No exceptions were thrown.")
-print("Execution continues.")
